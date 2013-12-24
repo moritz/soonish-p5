1d5412607169007b1ec9780eb451b836c31655f4
Moritz Lenz <moritz@faui2k3.org>
2013-12-24 10:49:18 +0100

    show search center in result page

--- lib/Soonish/Web/Event.pm
+++ lib/Soonish/Web/Event.pm
@@ -8,12 +8,14 @@ sub list {
     my $self = shift;
     my $zipcode  = $self->param('plz');
     my $distance = $self->param('d') // 50;
+    $self->stash(distance => $distance);
     my @events;
     if ($zipcode) {
         @events = $self->model->event->close_to(
             zipcode     => $zipcode,
             distance    => $distance,
         );
+        $self->stash(close_to => $self->model->resultset('Geo')->find($zipcode));
     }
     else {
         @events   = $self->model->event->search(
