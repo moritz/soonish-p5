042aca8d990c65fea511c188aad011d2708d84d9
Moritz Lenz <moritz@faui2k3.org>
2014-01-12 16:53:59 +0100

    UI safeguard for deleting channels

--- public/js/channel.js
+++ public/js/channel.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    $('.remove-channel').click(function() {
-        var id = $(this).attr('data-id');
+    function really_delete(id) {
         $.ajax('/channel/delete', {
             type: 'post',
             accepts: 'json',
@@ -8,6 +7,15 @@ $(document).ready(function() {
             success: function(res) {
                 $('.channel-' + id).remove();
             }
+        });
+    }
+    $('.remove-channel').click(function() {
+        var id      = $(this).attr('data-id');
+        var $dialog = $('#delete-dialog').children().clone();
+        $('.channel-' + id + ' .remove-channel').after($dialog);
+        $dialog.find('.yes').click(function() { really_delete(id) });
+        $dialog.find('.no').click(function() {
+            $dialog.remove();
         });
     });
 });
